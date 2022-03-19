ecd44dd78bdbb2b3da0107d1e913f461f8e444e7
gharmange <54421836+gharmange@users.noreply.github.com>
2022-03-19 11:13:15 -0400

    add well removal

--- Run_Deeptile.py
+++ Run_Deeptile.py
@@ -1,72 +1,39 @@
-import glob
 import os
-import sys
-import numpy as np
+import glob
 
 ##FILL FOLLOWING VARIABLES
 
 #path do a single tif file or directory with multiple tif Files
 
-TIFS = glob.glob('/Users/guillaumeharmange/Desktop/Test_Dat/tst_img.tif',recursive=True)
+TIFS = glob.glob("/home/gharm/Hemma_Scope_Images/*.tif",recursive=True)
 
-#path to final mask
-OutPath = '/Users/guillaumeharmange/Desktop/Test_Dat/tst_img_mask.tif'
 # which channel contains a nuclear stain to use for segmentation (number)
 NucleusChannel= 1
 
 # which channel contains a cytoplasmic stain to use for segmentation (number)
-CytoplasmChannel= 2
+CytoplasmChannel= 0
 
 #which objective did you use for these scans (note that if using something other than Shaffer Lab scope this will not work and you need to change the library in RunDeepCell function)
 objective = '10x'
 
-#specify sample type of sample you are using (TC or tissue)
-#type = 'tissue'
-algorithm = 'deepcell_mesmer'
+#specify sample algorith you are using (tissue: deepcell_mesmer, TC nucleus:deepcell_nuclear, TC cytoplasm :deepcell_cytoplasm )
+
+algorithm = 'deepcell_nuclear'
 
 # change this path to location ot DeepCellHelper
-#dc_helperPath = '/Users/guillaumeharmange/Documents/GitHub/DeepCellHelper'
 
-dc_tilePath = '/Users/guillaumeharmange/Documents/GitHub/DeepTile'
+dc_tilePath = '/home/gharm/DeepTile'
+
+
+# change tile size(default =10000)
+tileSize=10000
+
+# If your image contains the outline of a round well make this argument True if not say False (default False)
+Well = True
 
 ##SHOULD NOT NEED TO EDIT BELOW THIS LINE
+os.chdir('/home/gharm/DeepCell/DeepCellHelper/')
+from Cyto_Nuc_DeepCell import RunDeepTile
 
-#os.chdir(dc_helperPath)
-#from Cyto_Nuc_DeepCell import RunDeepCell
-#from dcHelper import tile_tif
-
-sys.path.append(dc_tilePath)
-
-from deeptile import DeepTile
-
-import tifffile
-
-
-#load images
-image = tifffile.imread(TIFS)
-#image = image[NucleusChannel-1,...]
-image =  np.stack((image[NucleusChannel-1,...],image[CytoplasmChannel-1,...]), axis =0)
-
-# Set Parmeters
-RESOLUTIONS = {'10x':1.3, '10X':1.3,'20x':.5,'20X':.5, '60x':.2167, '60X':.2167,'100x':.13,'100X':.13}
-resolution = RESOLUTIONS[objective]
-
-# Number of tiles
-_,h, w = image.shape
-wTile = np.int(np.ceil(w/(np.ceil(w/10000))))
-hTile = np.int(np.ceil(h/(np.ceil(h/10000))))
-
-nTilesX = np.uint8(np.ceil(w / wTile))
-nTilesY = np.uint8(np.ceil(h / hTile))
-
-print(nTilesY)
-print(nTilesX)
-
-# run DeepCell
-dt = DeepTile(image)
-
-dt.create_job(n_blocks=(nTilesY ,nTilesX), overlap=(0.1, 0.1), algorithm=algorithm , eval_parameters={"image_mpp":resolution})
-
-masks = dt.run_job()
-
-tifffile.imwrite(OutPath, np.uint32(masks[0,...]))
+for tif in TIFS:
+    RunDeepTile(tif,NucleusChannel,CytoplasmChannel,objective,algorithm,tileSize,Well)
